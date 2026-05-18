237c6c9957912c93cbfbccb60ccc98688955d1a2
certkrasnyj51-lang <certkrasnyj51@gmail.com>
2026-05-18 09:30:35 +0300

    Добавлены классы для экранов

---
+++ app/src/main/java/com/example/unipathapp/FilterActivity.java
@@ -0,0 +1,12 @@
+package com.example.unipathapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FilterActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+    }
+}
