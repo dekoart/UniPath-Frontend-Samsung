2e02ee09216276f4adad2d6bfda1ba28f3cffebc
certkrasnyj51-lang <certkrasnyj51@gmail.com>
2026-05-23 17:57:31 +0300

    Редактировал экран и добавил экран профиль и ввод баллов

--- app/src/main/java/com/example/unipathapp/FilterActivity.java
+++ app/src/main/java/com/example/unipathapp/FilterActivity.java
@@ -1,10 +1,14 @@
 package com.example.unipathapp;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FilterActivity extends AppCompatActivity {
+
+    ImageButton btnFavorite, btnAbout;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
