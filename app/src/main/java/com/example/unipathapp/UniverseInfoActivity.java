a4fdb6c73bc3eecf0b0fb6d99ed886e939aa0835
certkrasnyj51-lang <certkrasnyj51@gmail.com>
2026-05-23 21:38:44 +0300

    что то добавил

--- app/src/main/java/com/example/unipathapp/UniverseInfoActivity.java
+++ app/src/main/java/com/example/unipathapp/UniverseInfoActivity.java
@@ -1,12 +1,36 @@
 package com.example.unipathapp;
 
 import android.os.Bundle;
-
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UniverseInfoActivity extends AppCompatActivity {
+    ImageView imgFavorite;
+
+    boolean isLiked = false;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_universeinfo);
+
+        imgFavorite = findViewById(R.id.imgFavorite);
+
+        imgFavorite.setOnClickListener(v -> {
+
+            if (isLiked) {
+
+                imgFavorite.setImageResource(R.drawable.favoriteuni);
+                isLiked = false;
+
+            } else {
+
+                imgFavorite.setImageResource(R.drawable.favoriteuni_);
+                isLiked = true;
+
+            }
+
+        });
+
     }
 }
