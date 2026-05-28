8600ce53a35b39a7d02696cc45bd4221ba1e077d
certkrasnyj51-lang <certkrasnyj51@gmail.com>
2026-05-28 18:13:32 +0300

    class

--- app/src/main/java/com/example/unipathapp/api/RetrofitClient.kt
+++ app/src/main/java/com/example/unipathapp/api/RetrofitClient.kt
@@ -8,25 +8,24 @@ object RetrofitClient {
     private const val BASE_URL = "http://10.0.2.2:8080/"
 
     val authApi: AuthApi by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(AuthApi::class.java)
+        Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create()).build().create(AuthApi::class.java)
     }
 
     val scoresApi: ScoresApi by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create()).build()
             .create(ScoresApi::class.java)
     }
 
     val programApi: ProgramApi by lazy {
         Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
             .build().create(ProgramApi::class.java)
+    }
+
+
+    val universityApi: UniversityApi by lazy {
+        Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
+            .build().create(UniversityApi::class.java)
     }
 }
--- app/src/main/java/com/example/unipathapp/api/UniversityApi.kt
+++ app/src/main/java/com/example/unipathapp/api/UniversityApi.kt
@@ -1,7 +1,7 @@
 package com.example.unipathapp.api
 
-import com.example.unipathapp.models.UniversityResponse
 import com.example.unipathapp.models.UniversityFilterRequest
+import com.example.unipathapp.models.UniversityResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
